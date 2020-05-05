77d32d14d14f7f3734818b69f361c1f7883eaac3
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-05-05 19:17:40 +0300

    Add files via upload

---
+++ antiddos.php
@@ -0,0 +1,32 @@
+<?php
+require "data.php";
+$ip=$_SERVER['REMOTE_ADDR'];
+$nowtime = date("Y-m-d H:i:s");
+$oldtime = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s"))-$second );
+$url = $_SERVER['REQUEST_URI']; 
+$file = $_SERVER["DOCUMENT_ROOT"] . "/.htaccess";
+$result = mysqli_query($link, "INSERT INTO `$table` (`ip`,`time`) VALUES ('$ip','$nowtime')");
+$result = mysqli_query($link, "DELETE FROM `$table` WHERE `ip`='$ip' and `time` BETWEEN '2020-01-01 00:00:00' and '$oldtime'");
+$result = mysqli_query($link, "SELECT COUNT(*) FROM `$table` WHERE `ip`='$ip' and `time` BETWEEN '$oldtime' and '$nowtime'");
+if ($row=mysqli_fetch_row($result)){
+    $count = $row[0];
+    if ((int)$count > $limit){
+        $f = fopen($file, "a");
+        fwrite($f, "Deny from $ip\n" );
+        fclose($f);
+        $result = mysqli_query($link, "DELETE FROM `$table` WHERE `ip`='$ip'");
+    }
+}
+if (!empty($_GET)) {
+    $array = json_encode($_GET);
+    $f = fopen($f_GET, "a");
+    fwrite($f, "$nowtime | $ip $array $url\n" );
+    fclose($f);
+}
+if (!empty($_POST)) {
+    $array = json_encode($_POST);
+    $f = fopen($f_POST, "a");
+    fwrite($f, "$nowtime | $ip $array $url\n" );
+    fclose($f);
+}
+?>
---
+++ data.php
@@ -0,0 +1,26 @@
+<?php
+
+# MySQL
+$host = 'localhost';
+$database = ''; 
+$user = ''; 
+$password = ''; 
+$link = mysqli_connect($host, $user, $password, $database);
+$table = ''; //name your table ( I have for example "antiddos" )
+
+# rus # Ограничение по количеству запросов в заданное время, после которого ip уйдет в бан
+# eng # Limit on the number of requests at a certain time, after which the ip will be banned
+$limit = 20;
+
+# rus # Ограничение по времени, (за какой промежуток времени в секундах будет проверяться кол-во запросов($limit) )
+# eng # Time limit
+$second=60;
+
+# rus # Создайте папку и файлы в ней куда быдут записываться логи и пропишите путь к ним
+# rus # У меня к примеру папку logs и файлы get.log и post.log для записей логов GET и POST запросов соответственно.
+# eng # Create a folder and files in it where logs will be recorded and write the path to them
+# eng # I have for example the logs folder and the get.log and post.log files for log entries for GET and POST requests, respectively.
+$f_GET = $_SERVER["DOCUMENT_ROOT"] . "/logs/get.log";
+$f_POST = $_SERVER["DOCUMENT_ROOT"] . "/logs/post.log";
+
+?>
