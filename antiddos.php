22a6851dbce28f15d2de1a6765fbe341dc251131
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-05-06 18:40:55 +0300

    Update antiddos.php

--- antiddos.php
+++ antiddos.php
@@ -6,7 +6,7 @@ $oldtime = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s"))-$second );
 $url = $_SERVER['REQUEST_URI']; 
 $file = $_SERVER["DOCUMENT_ROOT"] . "/.htaccess";
 $result = mysqli_query($link, "INSERT INTO `$table` (`ip`,`time`) VALUES ('$ip','$nowtime')");
-$result = mysqli_query($link, "DELETE FROM `$table` WHERE `ip`='$ip' and `time` BETWEEN '2020-01-01 00:00:00' and '$oldtime'");
+$result = mysqli_query($link, "DELETE FROM `$table` WHERE `time` BETWEEN '2020-01-01 00:00:00' and '$oldtime'");
 $result = mysqli_query($link, "SELECT COUNT(*) FROM `$table` WHERE `ip`='$ip' and `time` BETWEEN '$oldtime' and '$nowtime'");
 if ($row=mysqli_fetch_row($result)){
     $count = $row[0];
