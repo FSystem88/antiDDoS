757bc7df98a292ae08a46679caeb384bc4d43d9b
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-11-30 19:41:51 +0300

    Update antiddos.php

--- antiddos.php
+++ antiddos.php
@@ -1,32 +1,33 @@
 <?php
 require "data.php";
 $ip=$_SERVER['REMOTE_ADDR'];
+$zapros = $_SERVER['REQUEST_METHOD'];
 $nowtime = date("Y-m-d H:i:s");
 $oldtime = date("Y-m-d H:i:s", strtotime(date("Y-m-d H:i:s"))-$second );
 $url = $_SERVER['REQUEST_URI']; 
 $file = $_SERVER["DOCUMENT_ROOT"] . "/.htaccess";
-$result = mysqli_query($link, "INSERT INTO `$table` (`ip`,`time`) VALUES ('$ip','$nowtime')");
-$result = mysqli_query($link, "DELETE FROM `$table` WHERE `time` BETWEEN '2020-01-01 00:00:00' and '$oldtime'");
-$result = mysqli_query($link, "SELECT COUNT(*) FROM `$table` WHERE `ip`='$ip' and `time` BETWEEN '$oldtime' and '$nowtime'");
+$f_LOG = $_SERVER["DOCUMENT_ROOT"] . "/.logs/log.log";
+
+$res= mysqli_query($link, "SELECT * FROM `onoff` WHERE `antiddos`='on'");
+
+$result = mysqli_query($link, "INSERT INTO `antiddos` (`ip`,`time`) VALUES ('$ip','$nowtime')");
+$result = mysqli_query($link, "DELETE FROM `antiddos` WHERE `time` BETWEEN '2020-01-01 00:00:00' and '{$oldtime}'");
+$result = mysqli_query($link, "SELECT COUNT(*) FROM `antiddos` WHERE `ip`='$ip' and `time` BETWEEN '{$oldtime}' and '{$nowtime}'");
 if ($row=mysqli_fetch_row($result)){
     $count = $row[0];
     if ((int)$count > $limit){
         $f = fopen($file, "a");
         fwrite($f, "Deny from $ip\n" );
         fclose($f);
-        $result = mysqli_query($link, "DELETE FROM `$table` WHERE `ip`='$ip'");
+        $result = mysqli_query($link, "DELETE FROM `antiddos` WHERE `ip`='$ip'");
     }
 }
-if (!empty($_GET)) {
-    $array = json_encode($_GET);
-    $f = fopen($f_GET, "a");
-    fwrite($f, "$nowtime | $ip $array $url\n" );
-    fclose($f);
-}
-if (!empty($_POST)) {
-    $array = json_encode($_POST);
-    $f = fopen($f_POST, "a");
-    fwrite($f, "$nowtime | $ip $array $url\n" );
-    fclose($f);
-}
+
+if ($zapros == "GET") { $array = json_encode($_GET); }
+elseif ($zapros == "POST") { $array = json_encode($_POST); }
+if (empty($array)) { $array = ""; }
+$f = fopen($f_LOG, "a");
+fwrite($f, "$nowtime | $zapros $ip $url $array\n" );
+fclose($f);
+
 ?>
